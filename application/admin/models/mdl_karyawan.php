1650c8bc6fbd96f248a3e82a62a7a46936170d0b
hamdi ahmadi muzakkiy <hamdiahmadi1504@gmail.com>
2015-06-09 23:02:01 +0700

    tmp

--- application/admin/models/mdl_karyawan.php
+++ application/admin/models/mdl_karyawan.php
@@ -46,7 +46,7 @@ class mdl_karyawan extends CI_Model{
             return $this->db->get("users");
         }
 	function insert($data)
-	{
+	{		
 		$this->db->flush_cache();
 		$this->db->insert('karyawan', $data);
 	}
